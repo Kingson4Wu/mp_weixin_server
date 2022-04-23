3358e51d3698d2ea219265b25c509ac108390c34
kingson4wu <kingson4wu@gmail.com>
2022-04-23 22:03:14 +0800

    v

--- admin/admin.go
+++ admin/admin.go
@@ -2,6 +2,6 @@ package admin
 
 func IsAdminstrator(account string) bool {
 
-	return account == "oqV-XjlEcZZcA4pCwoaiLtnFF0XQ"
+	return account == "oqV-XjlEcZZcA4pCwoaiLtnFF0XQ" || account == "oqV-Xju6thOVtzvi0FrTWHaB5So4"
 
 }
