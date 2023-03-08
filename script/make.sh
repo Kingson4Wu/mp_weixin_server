b045117b4413e8687de1aa6fa34aee764f6cf4c2
kingson4wu <kingson4wu@gmail.com>
2023-03-08 23:02:14 +0800

    optimise

--- script/make.sh
+++ script/make.sh
@@ -1,3 +1,5 @@
-env GOOS=linux GOARCH=arm64 go build  -o ~/Downloads/weixinapp ./cmd/main.go
+env GOOS=linux GOARCH=arm64 go build -ldflags="-s -w"  -o ~/Downloads/weixinapp ./cmd/main.go && upx -9 ~/Downloads/weixinapp
+
+# brew install upx
 
 #ps -ef|grep 'weixinapp'|grep -v 'grep'|awk '{ print $2}'|xargs kill -15
